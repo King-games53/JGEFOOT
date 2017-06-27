28fca7e7b6af4e1e84a8057ab4de869920dff8b1
Thierry CHAMPOT <thierry.champot@gmail.com>
2017-06-27 16:29:29 +0200

    manualy merged with thierry

--- app/controllers/PageCtrl.js
+++ app/controllers/PageCtrl.js
@@ -11,16 +11,8 @@ class PageCtrl {
     }
 
     postLogin(req, res) {
-<<<<<<< HEAD
         if(_.isEmpty(req.body.login)) {
             res.render('page/login', {
-=======
-        if(_.isEmpty(req.body)||
-            _.isEmpty(req.body.login)
-
-        ) {
-            res.render('index/index', {
->>>>>>> c1ec35a84c00a2f3045e487ed9d2bcf7b800c5e2
                 message: 'Merci de saisir votre login.'
             });
             return;
--- test/controllers/PageCtrl.js
+++ test/controllers/PageCtrl.js
@@ -1,73 +1,6 @@
 const expect = require('expect');
 const PageCtrl = require('../../app/controllers/PageCtrl');
 
-// describe("PageCtrl", () => {
-//     describe("#constructor", () => {
-//         it("Verification de la présence de la propriété loginService", () => {
-//             const pageCtrl = new PageCtrl(true);
-//
-//             expect(pageCtrl._loginService).toBe(true);
-//         });
-//     });
-//
-//     describe("#login", () => {
-//         it("je m'attends à voir le vue qui affiche le formulaire de login: la page/login", () => {
-//             const page = new PageCtrl();
-//
-//             const req = {
-//             };
-//
-//             const res = {
-//                 render: view => {
-//                     expect(view).toBe('index/index');
-//                 }
-//             };
-//
-//             page.login(req, res);
-//         });
-//     });
-//
-//     describe("#postLogin", () => {
-//         it("Je m'attends à ne rien recevoir et j'affiche le message 'Merci de saisir le login' ", () => {
-//             const pageCtrl = new PageCtrl();
-//
-//             const req = {
-//                 body: {
-//
-//                 }
-//             };
-//
-//             const res = {
-//                 render: (view, data) => {
-//                     expect(view).toBe('index/index');
-//                     expect(data.message).toBe('Merci de saisir votre login.')
-//                 }
-//             };
-//
-//             pageCtrl.postLogin(req, res);
-//         });
-//
-//         it("Je m'attends à recevoir un login vide et j'affiche le message 'Merci de saisir le login'", () => {
-//             const loginCtrl=new PageCtrl();
-//             const req={
-//                 body:{
-//                     login:''
-//                 }
-//
-//                 };
-//             const res={
-//                 render:(view,data)=>{
-//                     expect(view).toBe('index/index');
-//                     expect(data.message).toBe('Merci de saisir votre login.')
-//                 }
-//             };
-//         loginCtrl.postLogin(req, res);
-//
-//         });
-//
-//
-// });
-
     describe("PageCtrl",()=> {
         describe('#constructor', () => {
             it("Vérification que le controleur peut récupérer un objet membre envoyé par le login service", () => {
@@ -78,131 +11,107 @@ const PageCtrl = require('../../app/controllers/PageCtrl');
                 })
             })
         });
-<<<<<<< HEAD
     });
 
     describe("#login", () => {
         it("je m'attends à voir la vue qui affiche le formulaire de login: la page/login", () => {
             const page = new PageCtrl();
-=======
->>>>>>> c1ec35a84c00a2f3045e487ed9d2bcf7b800c5e2
 
-        describe("#postLogin",()=>{
-        it("Je m'attends à recevoir un login qui correspond à un membre et j'affiche le message 'Vous êtes connecté.'", () => {
-            const loginMemberCtrl = new PageCtrl();
-            const req = {
-                body: {
-                    login: 7
-                }
+            describe("#postLogin", () => {
+                it("Je m'attends à ne rien recevoir et j'affiche le message 'Merci de saisir votre login.' ", () => {
+                    const pageCtrl = new PageCtrl();
 
-<<<<<<< HEAD
-    describe("#postLogin", () => {
-        it("Je m'attends à ne rien recevoir et j'affiche le message 'Merci de saisir votre login.' ", () => {
-            const pageCtrl = new PageCtrl();
+                    const req = {
+                        body: {}
+                    };
+                    const res = {
+                        render: (view, data) => {
+                            expect(view).toBe('page/login');
+                            expect(data.message).toBe('Merci de saisir votre login.')
+                        }
+                    };
 
-            const req = {
-                body: { }
-=======
->>>>>>> c1ec35a84c00a2f3045e487ed9d2bcf7b800c5e2
-            };
-            const res = {
-                render: (view, data) => {
-<<<<<<< HEAD
-                    expect(view).toBe('page/login');
-                    expect(data.message).toBe('Merci de saisir votre login.')
-                }
-            };
+                    pageCtrl.postLogin(req, res);
+                });
 
-            pageCtrl.postLogin(req, res);
-        });
+                it("Je m'attends à recevoir un login vide et j'affiche le message 'Merci de saisir votre login.'", () => {
+                    const pageCtrl = new PageCtrl();
 
-        it("Je m'attends à recevoir un login vide et j'affiche le message 'Merci de saisir votre login.'", () => {
-            const pageCtrl = new PageCtrl();
+                    const req = {
+                        body: {
+                            login: ''
+                        }
+                    };
 
-            const req = {
-                body: {
-                    login : ''
-                }
-            };
+                    const res = {
+                        render: (view, data) => {
+                            expect(view).toBe('page/login');
+                            expect(data.message).toBe('Merci de saisir votre login.')
+                        }
+                    };
 
-            const res = {
-                render: (view, data) => {
-                    expect(view).toBe('page/login');
-                    expect(data.message).toBe('Merci de saisir votre login.')
-                }
-            };
+                    pageCtrl.postLogin(req, res);
+                });
 
-            pageCtrl.postLogin(req, res);
-        });
+                it("Je m'attends à recevoir un login qui ne correspond à aucun membre et j'affiche le message 'Login invalide.'", () => {
 
-        it("Je m'attends à recevoir un login qui ne correspond à aucun membre et j'affiche le message 'Login invalide.'", () => {
+                    const req = {
+                        body: {
+                            login: '7'
+                        }
+                    };
 
-            const req = {
-                body: {
-                    login : '7'
-                }
-            };
+                    const loginService = {
+                        login: (login) => {
+                            expect(login).toBe('7');
+                            return new Promise((resolve) => {
+                                resolve(false);
+                            });
+                        }
+                    };
 
-            const loginService = {
-                login: (login) => {
-                    expect(login).toBe('7');
-                    return new Promise((resolve) => {
-                        resolve(false);
-                    });
-                }
-            };
-=======
-                    expect(view).toBe('index/index');
-                    expect(data.message).toBe('Vous êtes connecté.');
-                }
-            };
-            loginMemberCtrl.postLogin(req, res)
-        });
+                    const pageCtrl = new PageCtrl(loginService);
+
+                    const res = {
+                        render: (view, data) => {
+                            expect(view).toBe('page/login');
+                            expect(data.message).toBe('Login invalide.')
+                        }
+                    };
+
+                    pageCtrl.postLogin(req, res);
+
+                });
+
+                it("Je m'attends à recevoir un login qui correspond à un membre enregistré et je le redirige vers la page d'accueil'", () => {
+
+                    const req = {
+                        body: {
+                            login: '7'
+                        }
+                    };
+
+                    const loginService = {
+                        login: (login) => {
+                            expect(login).toBe('7');
+                            return new Promise((resolve) => {
+                                resolve(true);
+                            });
+                        }
+                    };
+
+                    const pageCtrl = new PageCtrl(loginService);
+
+                    const res = {
+                        render: (view, data) => {
+                            expect(view).toBe('index/index');
+                            expect(data.message).toBe('Vous êtes connecté.');
+                        }
+                    };
+
+                    pageCtrl.postLogin(req, res);
+
+                });
+            });
         });
     });
->>>>>>> c1ec35a84c00a2f3045e487ed9d2bcf7b800c5e2
-
-            const pageCtrl = new PageCtrl(loginService);
-
-            const res = {
-                render: (view, data) => {
-                    expect(view).toBe('page/login');
-                    expect(data.message).toBe('Login invalide.')
-                }
-            };
-
-            pageCtrl.postLogin(req, res);
-
-        });
-
-        it("Je m'attends à recevoir un login qui correspond à un membre enregistré et je le redirige vers la page d'accueil'", () => {
-
-            const req = {
-                body: {
-                    login : '7'
-                }
-            };
-
-            const loginService = {
-                login: (login) => {
-                    expect(login).toBe('7');
-                    return new Promise((resolve) => {
-                        resolve(true);
-                    });
-                }
-            };
-
-            const pageCtrl = new PageCtrl(loginService);
-
-            const res = {
-                render: (view, data) => {
-                    expect(view).toBe('index/index');
-                    expect(data.message).toBe('Vous êtes connecté.');
-                }
-            };
-
-            pageCtrl.postLogin(req, res);
-
-    });
-});
-});
